15979f0e79e3eba7d47686b5fc7f37f92ed49404
Jonathan Falanga <BookGeekRR@outlook.com>
2021-07-15 16:05:19 -0600

    Add project files.

---
+++ Sedan.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VehicleApp
+{
+    class Sedan: Vehicle
+    {
+        private int wheels = 4;
+        private int maxPass = 5;
+        public override int Wheels
+        {
+            get { return wheels; }
+        }
+
+        public override int MaxPassengers
+        {
+            get { return maxPass; }
+        }
+
+    }
+}
